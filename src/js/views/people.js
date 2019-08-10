445c965b962d77019ecb9723231a74ba089c830e
Eleazar <erondon@gmail.com>
2019-08-09 21:13:48 -0400

    cards added to people

--- src/js/views/people.js
+++ src/js/views/people.js
@@ -17,35 +17,33 @@ export class People extends React.Component {
 								{!!store.people.results &&
 									store.people.results.map((people, i) => {
 										return (
-											<div key={i}>
-												<div className="col-md-3">
-													<div className="card">
-														<img
-															className="card-img-top"
-															src="https://via.placeholder.com/150"
-															alt="Card image cap"
-														/>
-														<div className="card-body">
-															<h5 className="card-title">{people.name}</h5>
-															<p className="card-text">
-																<p>
-																	<strong>Hair Color:</strong>{" "}
-																	{capitalize(people.hair_color)}
-																</p>
-																<p>
-																	<strong>Skin Color:</strong>{" "}
-																	{capitalize(people.skin_color)}
-																</p>
-																<p>
-																	<strong>Gender Color:</strong>{" "}
-																	{capitalize(people.gender)}
-																</p>
-																<p>
-																	<strong>Birth Year:</strong>{" "}
-																	{capitalize(people.birth_year)}
-																</p>
+											<div className="col-md-3" key={i}>
+												<div className="card">
+													<img
+														className="card-img-top"
+														src="https://via.placeholder.com/150"
+														alt="Card image cap"
+													/>
+													<div className="card-body">
+														<h5 className="card-title">{people.name}</h5>
+														<p className="card-text">
+															<p>
+																<strong>Hair Color:</strong>{" "}
+																{capitalize(people.hair_color)}
 															</p>
-														</div>
+															<p>
+																<strong>Skin Color:</strong>{" "}
+																{capitalize(people.skin_color)}
+															</p>
+															<p>
+																<strong>Gender Color:</strong>{" "}
+																{capitalize(people.gender)}
+															</p>
+															<p>
+																<strong>Birth Year:</strong>{" "}
+																{capitalize(people.birth_year)}
+															</p>
+														</p>
 													</div>
 												</div>
 											</div>
